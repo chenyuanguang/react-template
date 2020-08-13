96c1015f1bad0e2e60a4401f2456f82adeb60f80
chenyuanguang <chenyuanguang@cmcm.com>
2020-08-13 14:26:39 +0800

    refactor: 增加底部footer连接

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './app.css';
 import Footer from '@components/Footer';
+
 class App extends Component {
     constructor(props) {
         super(props);
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import styles from './index.module.less';
+
 export default () => {
     return (
         <footer className={styles.footer}>
             <ul>
                 <li>
-                    <a href=""></a>github
+                    <a href="https://github.com/chenyuanguang/react-template/tree/react-family-meals">
+                        github
+                    </a>
                 </li>
                 <li></li>
-                <li>blend-cli</li>
+                <li>
+                    <a href="https://github.com/chenyuanguang/blend-cli">
+                        blend-cli
+                    </a>
+                </li>
             </ul>
         </footer>
     );
